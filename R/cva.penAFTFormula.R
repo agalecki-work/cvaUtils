5aca09beaf073bd0e61c81e104351a7f6aaeb354
agalecki <agalecki@umich.edu>
2022-06-29 15:29:41 -0400

    Update cva.penAFTFormula.R

--- R/cva.penAFTFormula.R
+++ R/cva.penAFTFormula.R
@@ -141,19 +141,27 @@ UseMethod("minlossplot")
 #'
 #' @rdname penAFT.cva
 #' @export
-minlossplot.penAFT.cva <- function(x, ..., cv.type=c("min", "1se"))
-{
+minlossplot.penAFT.cva <- function(x, ..., cv.type = c("min", "1se"), 
+    plot.it = c("min", "all")) {
     alpha <- x$alpha
     cv.type <- match.arg(cv.type)
-    
+    if (cv.type == "1se") stop("minlossplot: cv.type = 1se not implemented \n")
     cv.type <- paste0("lambda.", cv.type)
-    print(cv.type)
+     
+    plot.it <- match.arg(plot.it)
+
     cvm <- sapply(x$modlist, function(mod) {
         mod$cv.err.linPred[mod$lambda == mod[[cv.type]]] # cvm -> cv.err.linPred
     })
     #plot(alpha, cvm, ylab="CV loss", ...)
     nx <- length(alpha)
-    matplot(cvm, type = c("b"), pch =1, col= 1:nx)
+    if (plot.it == "all") {
+    matplot(cvm, type = c("b"), pch =1, col= 1:nx, ylab = "err.linPred")
     legend("topleft", legend = 1:nx, col=1:nx, pch=1)
+    } else if (plot.it == "min") {
+    cvm2 <- apply(cvm, 2, min)
+    # print(cvm2)
+    plot (cvm2 ~ alpha, type = c("b"), ylab = "err.linPred_min", xlab = "lambda")
+    }
     invisible(x)
 }
