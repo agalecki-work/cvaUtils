0db61a402430be62f7cfa7beb00ffed91a677ff5
agalecki <agalecki@umich.edu>
2022-07-19 18:22:03 -0400

    R scriprs

--- inst/scriptsR4.1.3/03save_penAFT.R
+++ inst/scriptsR4.1.3/03save_penAFT.R
@@ -3,12 +3,40 @@ setwd("C:/temp")
 library(cvaUtils)
 rm(list =ls())
 str(proteins21)
-dtc <- proteins21
 
-# penAFTcva
+# Prep data
+dtc <- proteins21
 X  <- as.matrix(dtc[, 3:26])
 logY  <- log(dtc[, "time"]) 
 delta <- dtc[, "status"]   # status=0 indicates censored observation 
+
+
+#--  ?penAFT{penAFT]
+library(penAFT)
+
+# Fit the solution path for the regularized semiparametric AFT model
+
+# -----------------------------------------------
+# Fit elastic net penalized estimator
+# -----------------------------------------------
+penAFT.en <- penAFT(X = X, logY = logY, delta = delta,
+                   nlambda = 50, lambda.ratio.min = 0.01,
+                   penalty = "EN",
+                   alpha = 1)
+class(penAFT.en)# penAFT
+
+#-- ?penAFT.cv{penAFT}
+
+penAFT.en.cv <- penAFT.cv(X = X, logY = logY, delta = delta,
+                   nlambda = 10, lambda.ratio.min = 0.1,
+                   penalty = "EN", nfolds = 5,
+                   alpha = 1)
+
+class(penAFT.en.cv)# penAFT.cv
+
+
+# penAFTcva
+
 alpha <- seq(0, 1, len = 11)^3
 penAFT.en.cva <- penAFT.cva(x=X, logY = logY, delta = delta,
                alpha = alpha,
--- inst/scriptsR4.1.3/11penAFT_cva.R
+++ inst/scriptsR4.1.3/11penAFT_cva.R
@@ -16,14 +16,19 @@ class(penAFT.en.cva)
 xnew <- as.matrix(proteins21[1:5, 3:26])
 alpha <- seq(0, 1, len = 11)^3
 
-# Extract model fit(s) for selected alpha
 
+
+# How extract model fit(s) from cva object for selected alpha
+# ... and calculate predicted values
 which.alpha <- 3
-mod <- penAFT.en.cva$modlist[[which.alpha]]
-str(mod)
+mod.cv <- penAFT.en.cva$modlist[[which.alpha]]
+class(mod.cv) # penAFT.cv
+str(mod.cv)
+
+# Calculate 
 penAFT::penAFT.predict(mod, Xnew =xnew, lambda = mod$full.fit$lmbda[10])
 
-
+# Calculate 
 predict(penAFT.en.cva, newx=xnew, alpha = alpha[which.alpha])
 minlossplot(penAFT.en.cva)
 lossdf <- lossgrid(penAFT.en.cva)
